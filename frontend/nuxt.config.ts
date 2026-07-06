55e278d5573cc6cac310404b1f1f1fcc69fc3215
DanielaListh <dani_moe_berrios@hotmail.com>
2026-07-06 11:59:40 -0400

    import fonts movido desde css a defineNuxtConfig

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -9,7 +9,10 @@ export default defineNuxtConfig({
     ]
   },
 
-  css: ['~/assets/css/tailwind.css'],
+  css: [
+    '~/assets/css/tailwind.css',
+    'https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&family=Aboreto&family=Bellefair&family=Cinzel:wght@400..900&family=Coiny&family=Corinthia:wght@400;700&family=Lexend+Exa:wght@100..900&display=swap'
+  ],
 
   postcss: {
     plugins: {
